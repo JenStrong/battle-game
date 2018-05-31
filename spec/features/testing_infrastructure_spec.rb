5c575a1a9af72e22e7c64f2a53680189e99115c7
Jen Strong <jenniferastrong@gmail.com>
2018-05-31 14:47:25 +0100

    Viewing hit points

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -7,3 +7,13 @@ feature 'Entering names' do
     expect(page).to have_content "Jen vs. Jon"
   end
 end
+
+feature 'View hit points' do
+  scenario 'see Player 2 hit points' do
+    visit('/')
+    fill_in :player_1_name, with: "Jen"
+    fill_in :player_2_name, with: "Jon"
+    click_button "Submit"
+    expect(page).to have_content "Jon: 60HP"
+  end
+end
